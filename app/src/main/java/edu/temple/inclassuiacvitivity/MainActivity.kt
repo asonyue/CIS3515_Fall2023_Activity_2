1b8d29234ae43ef51f1cc83601887d8c164c791e
asonyue <410856065@gms.tku.edu.tw>
2023-09-19 12:02:33 -0400

    Added the functionality of the toggle list to change to font size of the text provided

--- app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
@@ -14,14 +14,22 @@ class MainActivity : AppCompatActivity() {
         val displayTextView = findViewById<TextView>(R.id.textDisplay)
 
         /* Step 1: Populate this array */
-        //val numberArray = Array Of Numbers
+        val numberArray = (1..100).toList()
 
         /* Step 2: Create adapter to display items from array in Spinner */
-        //spinner.adapter = ArrayAdapter...
-
+        spinner.adapter = ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, numberArray)
 
         // Step 3: Change TextView's text size to the number selected in the Spinner */
-        //spinner.onItemSelectedListener = object: ...
+        spinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                val selectedNumber = numberArray[p2]
+                displayTextView.textSize = selectedNumber.toFloat()
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+//                Leave it Empty
+            }
+        }
 
     }
 }
